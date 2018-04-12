57bf4da3f3a3b6e2aa98e41779b1c22e92ff8737
Kazutomo Niwa <niwa_kazutomo@gs2.io>
2018-04-12 23:16:19 +0900

    リファクタリング

--- src/gs2_script_client/control/DeleteScriptRequest.py
+++ src/gs2_script_client/control/DeleteScriptRequest.py
@@ -49,7 +49,7 @@ class DeleteScriptRequest(Gs2BasicRequest):
         :param script_name: スクリプトの名前を指定します。
         :type script_name: unicode
         """
-        if not isinstance(script_name, unicode):
+        if script_name and not isinstance(script_name, unicode):
             raise TypeError(type(script_name))
         self.__script_name = script_name
 
